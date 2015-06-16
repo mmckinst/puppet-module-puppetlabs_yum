5adf55eb8ab517d2ea6da4cbc574136c2fbbcb65
Mark McKinstry <mmckinst@nexcess.net>
2015-06-16 12:49:12 -0400

    it might be a boolean too

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,7 +34,7 @@ class puppetlabs_yum (
   $enable_collection         = false,
 ) inherits puppetlabs_yum::params {
 
-  if $::is_pe == 'false' or $::is_pe == undef {
+  if $::is_pe == 'false' or $::is_pe == false or $::is_pe == undef {
 
     if $::osfamily == 'RedHat' {
       include puppetlabs_yum::gpg_key
